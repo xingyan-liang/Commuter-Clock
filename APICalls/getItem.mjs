1d2c4650e04e45935eb33367c01797bf3a2a635e
Jonathan <jonmaingot@gmail.com>
2025-01-10 17:22:21 -0500

    Updated API Lambda function
    Added full CRUD functionality framework. Will need updates later

--- APICalls/getItem.mjs
+++ APICalls/getItem.mjs
@@ -1,35 +1,82 @@
-export const handler = async (event) => {
-  
-    const items = {
-      1: {
-        name: 'Item 1',
-        description: 'This is item 1',
-      },
-      2: {
-        name: 'Item 2',
-        description: 'This is item 2',
-      },
-      3: {
-        name: 'Item 3',
-        description: 'This is item 3',
-      }
-    }
-  
-    const itemID = event.pathParameters.itemID;
-  
-    const item = items[itemID];
-  
-    if(!item) {
-      return {
-        statusCode: 404,
-        body: JSON.stringify({ message: 'Item not found for ${itemID}' })
-      };
-    }
-  
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(item),
-    };
-    return response;
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import {
+  DynamoDBDocumentClient,
+  ScanCommand,
+  PutCommand,
+  GetCommand,
+  DeleteCommand,
+} from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+
+const dynamo = DynamoDBDocumentClient.from(client);
+
+const tableName = "AlarmTable";
+
+export const handler = async (event, context) => {
+  let body;
+  let statusCode = 200;
+  const headers = {
+    "Content-Type": "application/json",
   };
-  
+
+  try {
+    switch (event.routeKey) {
+      case "DELETE /users/{UserID}":
+        await dynamo.send(
+          new DeleteCommand({
+            TableName: tableName,
+            Key: {
+              UserID: event.pathParameters.UserID,
+            },
+          })
+        );
+        body = `Deleted item ${event.pathParameters.UserID}`;
+        break;
+      case "GET /users/{UserID}":
+        body = await dynamo.send(
+          new GetCommand({
+            TableName: tableName,
+            Key: {
+              UserID: event.pathParameters.UserID,
+            },
+          })
+        );
+        body = body.Item;
+        break;
+      case "GET /users":
+        body = await dynamo.send(
+          new ScanCommand({ TableName: tableName })
+        );
+        body = body.Items;
+        break;
+      case "PUT /users":
+        let requestJSON = JSON.parse(event.body);
+        await dynamo.send(
+          new PutCommand({
+            TableName: tableName,
+            Item: {
+              UserID: requestJSON.UserID,
+              price: requestJSON.price,
+              name: requestJSON.name,
+            },
+          })
+        );
+        body = `Put item ${requestJSON.UserID}`;
+        break;
+      default:
+        throw new Error(`Unsupported route: "${event.routeKey}"`);
+    }
+  } catch (err) {
+    statusCode = 400;
+    body = err.message;
+  } finally {
+    body = JSON.stringify(body);
+  }
+
+  return {
+    statusCode,
+    body,
+    headers,
+  };
+};
