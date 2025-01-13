ff450935407e7df0583908c3d561dbe9e1c908bd
Jonathan <jonmaingot@gmail.com>
2025-01-13 15:37:45 -0500

    updated lamdba
    Added ability to add new alarms and made the put not need item format

--- APICalls/getItem.mjs
+++ APICalls/getItem.mjs
@@ -5,6 +5,7 @@ import {
   PutCommand,
   GetCommand,
   DeleteCommand,
+  UpdateCommand,
 } from "@aws-sdk/lib-dynamodb";
 
 const client = new DynamoDBClient({});
@@ -22,7 +23,7 @@ export const handler = async (event, context) => {
 
   try {
     switch (event.routeKey) {
-      case "DELETE /users/{UserID}":
+      case "DELETE /users/{UserID}": //deletes user based on ID
         await dynamo.send(
           new DeleteCommand({
             TableName: tableName,
@@ -33,7 +34,8 @@ export const handler = async (event, context) => {
         );
         body = `Deleted item ${event.pathParameters.UserID}`;
         break;
-      case "GET /users/{UserID}":
+
+      case "GET /users/{UserID}": //gets single user
         body = await dynamo.send(
           new GetCommand({
             TableName: tableName,
@@ -44,21 +46,50 @@ export const handler = async (event, context) => {
         );
         body = body.Item;
         break;
-      case "GET /users":
+
+      case "GET /users": //gets all users(probably delete this)
         body = await dynamo.send(
           new ScanCommand({ TableName: tableName })
         );
         body = body.Items;
         break;
-      case "PUT /users":
+
+      case "POST /users/{UserID}/alarms": //add a new alarm
+        let alarmJSON = JSON.parse(event.body);
+      
+        // Validate the request body
+        if (!alarmJSON.newAlarm) {
+          throw new Error("Missing required field: newAlarm");
+        }
+      
+        await dynamo.send(
+          new UpdateCommand({
+            TableName: tableName,
+            Key: {
+              UserID: event.pathParameters.UserID,
+            },
+            UpdateExpression: "SET #Alarms = list_append(if_not_exists(#Alarms, :emptyList), :newAlarm)",
+            ExpressionAttributeNames: {
+              "#Alarms": "Alarms", //remaps alarms as it is a reserved word
+            },
+            ExpressionAttributeValues: {
+              ":newAlarm": [alarmJSON.newAlarm], // Wrap the new alarm in an array
+              ":emptyList": [], // Fallback if Alarms attribute doesn't exist
+            },
+            ReturnValues: "UPDATED_NEW", // Return the updated Alarms array
+          })
+        );
+      
+        body = `Added alarm "${alarmJSON.newAlarm}" to user ${event.pathParameters.UserID}`;
+        break;
+
+      case "PUT /users": //add a new user or update
         let requestJSON = JSON.parse(event.body);
         await dynamo.send(
           new PutCommand({
             TableName: tableName,
             Item: {
-              UserID: requestJSON.UserID,
-              price: requestJSON.price,
-              name: requestJSON.name,
+              ...requestJSON, // Spread the request body object into the Item
             },
           })
         );
