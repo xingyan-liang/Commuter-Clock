c095bed226cc68b43957a3b629774ad9d8ca73c0
Jonathan <jonmaingot@gmail.com>
2025-01-13 16:24:56 -0500

    added nodejs test backend and CRUD calls

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { registerRootComponent } from 'expo';
-import fetch from 'node-fetch';
+import axios from 'axios';
 const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 
@@ -8,16 +8,40 @@ const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 import App from './App';
 
-const fetchItems = async () => {
+const getUsers = async () => {
+    
   try {
-    const response = await fetch(`${apiUrl}/items/123`);  // Use the API URL
-    const data = await response.json();
-    console.log('Items:', data);
+    const response = await axios.get(`${apiUrl}/users`);
+    console.log("User data:", response.data);
   } catch (error) {
-    console.error('Error fetching items:', error);
+    console.error("Error fetching users:", error.message);
   }
 };
 
+
+const getUser = async (userID) => {
+  
+  try {
+    const response = await axios.get(`${apiUrl}/users/${userID}`);
+    console.log("User data:", response.data);
+  } catch (error) {
+    console.error("Error fetching user:", error.message);
+  }
+};
+
+const putUser = async () => {
+  
+  try {
+    const response = await axios.put(`${apiUrl}/users`, {UserID: "jimmy", Password: "password"}, {
+      headers: {
+        "Content-Type": "application/json"
+      },
+  });
+    console.log("User data:", response.data);
+  } catch (error) {
+    console.error("Error putting user:", error.message);
+  }
+};
 
 
 
@@ -26,5 +50,7 @@ const fetchItems = async () => {
 // the environment is set up appropriately
 registerRootComponent(App);
 
-fetchItems();
+getUsers();
+getUser("jeff");
+putUser();
 
