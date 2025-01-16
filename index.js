8ff0c0b5f18f02a51914039f0fdceb97ec738eaa
Jonathan <jonmaingot@gmail.com>
2025-01-16 16:25:01 -0500

    took out duplicate code

--- index.js
+++ index.js
@@ -8,40 +8,6 @@ const apiUrl = process.env.EXPO_PUBLIC_API_URL;
 
 import App from './App';
 
-const getUsers = async () => {
-    
-  try {
-    const response = await axios.get(`${apiUrl}/users`);
-    console.log("User data:", response.data);
-  } catch (error) {
-    console.error("Error fetching users:", error.message);
-  }
-};
-
-
-const getUser = async (userID) => {
-  
-  try {
-    const response = await axios.get(`${apiUrl}/users/${userID}`);
-    console.log("User data:", response.data);
-  } catch (error) {
-    console.error("Error fetching user:", error.message);
-  }
-};
-
-const putUser = async () => {
-  
-  try {
-    const response = await axios.put(`${apiUrl}/users`, {UserID: "jimmy", Password: "password"}, {
-      headers: {
-        "Content-Type": "application/json"
-      },
-  });
-    console.log("User data:", response.data);
-  } catch (error) {
-    console.error("Error putting user:", error.message);
-  }
-};
 
 
 
@@ -49,8 +15,4 @@ const putUser = async () => {
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 registerRootComponent(App);
-
-getUsers();
-getUser("jeff");
-putUser();
 
